0711e9d0ed5103294b8cd8f4873a7f82775fef55
oftfrfbf <rudyklucik@gmail.com>
2026-03-06 20:24:36 -0700

    created card component and added them to a card group

--- src/view/GraphView/GraphCard.jsx
+++ src/view/GraphView/GraphCard.jsx
@@ -1,46 +1,85 @@
-import { useEffect, useRef } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
-import Form from "react-bootstrap/Form";
+import PropTypes from "prop-types";
 
-export default function GraphCard() {
+function GraphCard({
+  classification="fish_school",
+  ship="Henry_B._Bigelow",
+  cruise="HB1906",
+  instrument="EK60",
+  startTime="2025-02-06 10:50:00",
+  endTime="2025-02-06 10:51:00",
+  localTime="2019-09-25T10:02:06.60",
+  longitude=-69.80750,
+  latitude=41.79967,
+  depthMin=11.12,
+  depthMax=13.15,
+  altitude=1.23,
+  distanceFromCoast=250,
+  solarAltitude=35.02,
+  phaseOfDay="day",
+  geometryHash="1d4b073ae6dda69972e42d4f707134640c1897c63b04c620d355dc527f7ef4ed",
+}) {
   return (
     <div className="GraphCard">
-      <Card style={{ width: "18rem" }} className="mx-auto">
+      <Card
+        style={{ width: '18rem' }}
+        bg="light"
+        text='dark'
+        className="m-2"
+      >
         <Card.Img
           variant="top"
-          src="https://noaa-wcsd-pds-index.s3.us-east-1.amazonaws.com/thumbnails/1d4b073ae6dda69972e42d4f707134640c1897c63b04c620d355dc527f7ef4ed.jpeg"
+          src={`https://noaa-wcsd-pds-index.s3.us-east-1.amazonaws.com/thumbnails/${geometryHash}.jpeg`}
         />
         <Card.Body>
-          <Card.Title className="text-center">fish_school</Card.Title>
           <Card.Text className="text-center">
-            Henry_B._Bigelow / HB1906 / EK60
+            <strong>&quot;{classification}&quot;</strong>
             <br />
-            2025-02-06 10:50:00 / 2025-02-06 10:51:00
+            {ship} | {cruise} | {instrument}
             <br />
-            depth: [11.12, 13.14]
+            {startTime} to {endTime}
             <br />
-            altitude: 1.23 m<br />
-            gps: -69.80750° E, 41.79967° N<br />
-            distance from coast: 250 m<br />
-            local time: 2019-09-25T10:02:06.60
+            (local time: {localTime})
             <br />
-            solar altitude: 35.02°
+            coordinates: {longitude}° E, {latitude}° N
             <br />
-            phase of day: day
+            depth: [{depthMin}, {depthMax}] m | altitude: {altitude} m
             <br />
-            <Link
-              to="/water-column?ship=Henry_B._Bigelow&cruise=HB1906&sensor=EK60&frequency=0&color=2&time=3974082"
+            distance from coast: {distanceFromCoast} m
+            <br />
+            solar altitude: {solarAltitude}° | phase: {phaseOfDay}
+            <br />
+            {/* TODO: need to convert time to ping_time */}
+            <Card.Link
+              href={`/water-column?ship=${ship}&cruise=${cruise}&sensor=${instrument}&frequency=0&color=2&time=3974082`}
               target="_blank"
             >
               View in the water column →
-            </Link>
+            </Card.Link>
           </Card.Text>
         </Card.Body>
       </Card>
     </div>
   );
 }
+
+export default GraphCard;
+
+GraphCard.propTypes = {
+  classification: PropTypes.string.isRequired,
+  ship: PropTypes.string.isRequired,
+  cruise: PropTypes.string.isRequired,
+  instrument: PropTypes.string.isRequired,
+  startTime: PropTypes.string.isRequired,
+  endTime: PropTypes.string.isRequired,
+  localTime: PropTypes.string.isRequired,
+  longitude: PropTypes.number.isRequired,
+  latitude: PropTypes.number.isRequired,
+  depthMin: PropTypes.number.isRequired,
+  depthMax: PropTypes.number.isRequired,
+  altitude: PropTypes.number.isRequired,
+  distanceFromCoast: PropTypes.number.isRequired,
+  solarAltitude: PropTypes.number.isRequired,
+  phaseOfDay: PropTypes.string.isRequired,
+  geometryHash: PropTypes.string.isRequired,
+};
---
+++ src/view/GraphView/GraphForm.jsx
@@ -0,0 +1,21 @@
+import Card from "react-bootstrap/Card";
+// import { Link } from "react-router-dom";
+// import PropTypes from "prop-types";
+
+function GraphForm() {
+  return (
+    <div className="GraphForm">
+      <Card style={{ width: "20rem" }} className="mx-auto">
+        <Card.Body>
+          <Card.Text className="text-center">
+            Hello
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default GraphForm;
+
+GraphForm.propTypes = {};
